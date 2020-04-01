e046e76072142d46c570b0ac8c5f9609f587cdaa
David Shriver <dls2fc@virginia.edu>
2020-04-01 18:42:10 -0400

    fix typo

--- scripts/run_verification.sh
+++ scripts/run_verification.sh
@@ -12,5 +12,5 @@ results_csv=$1; shift
 artifact_dir=$1; shift
 verifier=$1; shift
 
-echo "python -u ./tools/run_verification.py $results_csv $artifact_dif $verifier -M 64G -n 1 $@"
-python -u ./tools/run_verification.py $results_csv $artifact_dif $verifier -M 64G -n 1 $@
+echo "python -u ./tools/run_verification.py $results_csv $artifact_dir $verifier -M 64G -n 1 $@"
+python -u ./tools/run_verification.py $results_csv $artifact_dir $verifier -M 64G -n 1 $@
