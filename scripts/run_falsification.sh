a7066aa981f39856e3e23240f7a933699a47ba36
David Shriver <dls2fc@virginia.edu>
2020-04-01 18:36:15 -0400

    run scripts

---
+++ scripts/run_falsification.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+#SBATCH --cpus-per-task=8
+
+. .env/openenv.sh
+
+ulimit -Ss 128000
+
+results_csv=$1; shift
+artifact_dir=$1; shift
+
+echo "python -u ./tools/run_falsification.py $results_csv $artifact_dif -M 64G -n 1 $@"
+python -u ./tools/run_falsification.py $results_csv $artifact_dif -M 64G -n 1 $@
